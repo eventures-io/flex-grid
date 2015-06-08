c5f8709703b936550797bb12c9d8f59205a361fe
Peter Symoens <peter.symoens@eventures.io>
2015-06-08 16:41:05 +0100

    init

---
+++ client/app/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+angular.module('animatedGridApp', [
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
