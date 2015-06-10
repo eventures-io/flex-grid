440f9444610403048bc108797cafd55fe5fb5c2f
Peter Symoens <peter.symoens@eventures.io>
2015-06-10 23:12:20 +0100

    add filters and styling

--- client/app/app.js
+++ client/app/app.js
@@ -2,16 +2,30 @@
 
 
 angular.module('feedReaderApp', [
-  'ngSanitize',
-  'ui.router'
+        'ngSanitize',
+        'ui.router'
     ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $urlRouterProvider
-      .otherwise('/');
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider
+            .otherwise('/');
 
-    $locationProvider.html5Mode(true);
-  }).filter('html', function($sce) {
-    return function(val) {
-        return $sce.trustAsHtml(val);
+        $locationProvider.html5Mode(true);
+    }).filter('html',function ($sce) {
+        return function (val) {
+            return $sce.trustAsHtml(val);
+        };
+    }).filter('moment', function () {
+        return function (val) {
+            var date = moment(val);
+            return date.format('DD MMM YYYY');
+        };
+    }).filter('readingTime', function () {
+    return function (val) {
+        if(val) {
+        var minutes = Math.floor(val.split(' ').length / 200 )
+        if(minutes === 0) minutes = 1
+        return minutes;
+        }
     };
 });
+
